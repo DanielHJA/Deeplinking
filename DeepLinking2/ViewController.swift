c75f60e0ec1d68501666cd2bca94d39e069b9d6c
Daniel Hjartstrom <daniel.hjartstrom@consid.se>
2020-06-23 09:53:00 +0200

    Initial Commit

---
+++ DeepLinking2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DeepLinking2
+//
+//  Created by Daniel Hjärtström on 2020-06-23.
+//  Copyright © 2020 Daniel Hjärtström. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
