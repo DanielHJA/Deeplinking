21e15f0311c4a7e675aad718200fc033bab9f6de
Daniel Hjartstrom <daniel.hjartstrom@consid.se>
2020-08-10 09:16:04 +0200

    deep linking

--- DeepLinking2/ViewController.swift
+++ DeepLinking2/ViewController.swift
@@ -9,12 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private lazy var button: UIButton = {
+        let temp = UIButton()
+        temp.setTitle("Go to first app", for: .normal)
+        temp.setTitleColor(UIColor.white, for: .normal)
+        temp.addTarget(self, action: #selector(didTouchButton), for: .touchUpInside)
+        temp.backgroundColor = UIColor.blue
+        view.addSubview(temp)
+        temp.translatesAutoresizingMaskIntoConstraints = false
+        temp.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        temp.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        temp.heightAnchor.constraint(equalToConstant: 50.0).isActive = true
+        temp.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        return temp
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        button.isHidden = false
     }
 
+    @objc private func didTouchButton() {
+        UIApplication.openAppWith(identifier: "myFirstApp://", fallback: "https://kiloloco.com")
+    }
 
+}
+
+extension UIApplication {
+    static func openAppWith(identifier: String, fallback: String) {
+        let application = UIApplication.shared
+        let message = "Message from the second app!".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+        guard let websiteURL = URL(string: fallback) else { return }
+        
+        if let appURL = URL(string: "\(identifier)\(message)") {
+            if application.canOpenURL(appURL) {
+                application.open(appURL)
+            } else {
+                application.open(websiteURL)
+            }
+        } else {
+            application.open(websiteURL)
+        }
+    }
 }
 
